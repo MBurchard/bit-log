490fa57e69a129e6ffd802e35a952a71c86ecc9a
Martin Burchard <dev@burchard-it.de>
2024-02-13 11:26:33 +0100

    utility functions

---
+++ src/__tests__/utils.spec.ts
@@ -0,0 +1,53 @@
+import {LogLevel} from '../definitions';
+import {formatISO8601, formatLogLevel, truncateMiddle, truncateOrExtend} from '../utils';
+
+describe('test utils', () => {
+  it('formatLogLevel TRACE', () => {
+    expect(formatLogLevel(LogLevel.TRACE)).toBe('\x1B[90mTRACE\x1B[m');
+  });
+
+  it('formatLogLevel DEBUG', () => {
+    expect(formatLogLevel(LogLevel.DEBUG)).toBe('\x1B[37mDEBUG\x1B[m');
+  });
+
+  it('formatLogLevel INFO', () => {
+    expect(formatLogLevel(LogLevel.INFO)).toBe('\x1B[92mINFO\x1B[m');
+  });
+
+  it('formatLogLevel WARN', () => {
+    expect(formatLogLevel(LogLevel.WARN)).toBe('\x1B[93mWARN\x1B[m');
+  });
+
+  it('formatLogLevel ERROR', () => {
+    expect(formatLogLevel(LogLevel.FATAL)).toBe('\x1B[95mFATAL\x1B[m');
+  });
+
+  it('formatLogLevel FATAL', () => {
+    expect(formatLogLevel(LogLevel.FATAL)).toBe('\x1B[95mFATAL\x1B[m');
+  });
+
+  it('formatLogLevel FATAL', () => {
+    expect(formatLogLevel(LogLevel.OFF)).toBe('');
+  });
+
+  it('formatISO8601', () => {
+    const date = new Date();
+    expect(formatISO8601(date)).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}[+-]\d{2}:\d{2}$/);
+  });
+
+  it('truncateMiddle if string is shorter then the given limit', () => {
+    expect(truncateMiddle('to short', 10)).toBe('to short');
+  });
+
+  it('truncateMiddle if string has exactly the same length as the given limit', () => {
+    expect(truncateMiddle('exactly 10', 10)).toBe('exactly 10');
+  });
+
+  it('truncateMiddle if string is longer then the given limit', () => {
+    expect(truncateMiddle('longer then', 10)).toBe('long...hen');
+  });
+
+  it('truncateOrExtend if string is shorter then the given limit', () => {
+    expect(truncateOrExtend('to short', 10)).toBe('to short  ');
+  });
+});
---
+++ src/utils.ts
@@ -0,0 +1,119 @@
+import {LogLevel} from './definitions';
+
+/**
+ * Format the given timestamp in the typical ISO-8601 format `YYYY-MM-DDTHH:mm:ss.SSSXXX`.
+ *
+ * If a more flexibel solution is needed, dayjs can be used.
+ * However, I would always use a timestamp that makes it clear in which time zone the system was located.
+ *
+ * @param date
+ * @return formatted string
+ */
+export function formatISO8601(date: Date): string {
+  const YYYY = date.getFullYear();
+  const MM = String(date.getMonth() + 1).padStart(2, '0');
+  const DD = String(date.getDate()).padStart(2, '0');
+  const HH = String(date.getHours()).padStart(2, '0');
+  const mm = String(date.getMinutes()).padStart(2, '0');
+  const ss = String(date.getSeconds()).padStart(2, '0');
+  const SSS = String(date.getMilliseconds()).padStart(3, '0');
+
+  const timeZoneOffset = date.getTimezoneOffset();
+  const offsetSign = timeZoneOffset > 0 ? '-' : '+';
+  const offsetHours = String(Math.abs(Math.floor(timeZoneOffset / 60))).padStart(2, '0');
+  const offsetMinutes = String(Math.abs(timeZoneOffset) % 60).padStart(2, '0');
+  const XXX = `${offsetSign}${offsetHours}:${offsetMinutes}`;
+
+  return `${YYYY}-${MM}-${DD}T${HH}:${mm}:${ss}.${SSS}${XXX}`;
+}
+
+/**
+ * use ANSI color codes to colorize the string showing the loglevel
+ *
+ * @internal
+ * @param level
+ */
+export function formatLogLevel(level: LogLevel): string {
+  switch (level) {
+    case LogLevel.DEBUG:
+      return '\x1B[37mDEBUG\x1B[m';
+    case LogLevel.ERROR:
+      return '\x1B[91mERROR\x1B[m';
+    case LogLevel.FATAL:
+      return '\x1B[95mFATAL\x1B[m';
+    case LogLevel.INFO:
+      return '\x1B[92mINFO\x1B[m';
+    case LogLevel.TRACE:
+      return '\x1B[90mTRACE\x1B[m';
+    case LogLevel.WARN:
+      return '\x1B[93mWARN\x1B[m';
+    default:
+      return '';
+  }
+}
+
+/**
+ * Format the prefix of each log entry.
+ * As usual, the timestamp, the log level and the category are used.
+ *
+ * @internal
+ * @param level
+ * @param name
+ */
+export function formatPrefix(level: LogLevel, name: string): string {
+  // let func = 'undefined';
+  // let src = 'undefined';
+  // let line = '';
+  // try {
+  //   const stack = (new Error()).stack;
+  //   if (stack) {
+  //     // console.error('Stack:', stack);
+  //     const stackLine = stack.split('\n')[4];
+  //     // console.error('Line:', stackLine);
+  //     if (stackLine) {
+  //       const match = stackLine.match(/^.*?at +?(.+?) .+?\.\/(src.+?)(?:\?.*?)?:(\d+).*?$/);
+  //       if (match) {
+  //         func = match[1] ?? 'undefined';
+  //         src = match[2] ?? 'undefined';
+  //         line = match[3] ?? '';
+  //       }
+  //     }
+  //   }
+  // } catch (e) {}
+  // return `${getTs()} ${formatLogLevel(level)} [${strPad(category, 20)}|${strPad(func, 20)}|${strPad(src, 30)}|${line.padStart(5, ' ')}]:`;
+  return `${formatISO8601(new Date())} ${formatLogLevel(level)} [${truncateOrExtend(name, 20)}]:`;
+}
+
+/**
+ * Truncates a string in the middle if it exceeds the desired length.
+ * Optionally, a custom replacement string can be provided for the middle part.
+ *
+ * @param str The input string to be truncated.
+ * @param length The desired maximum length of the resulting string.
+ * @param replacement The replacement string for the middle part (default is '...').
+ */
+export function truncateMiddle(str: string, length: number, replacement: string = '...'): string {
+  if (str.length <= length) {
+    return str;
+  }
+
+  const replacementLength = replacement.length;
+  const charsToShow = length - replacementLength;
+  const frontChars = Math.ceil(charsToShow / 2);
+  const backChars = Math.floor(charsToShow / 2);
+
+  return str.slice(0, frontChars) + replacement + str.slice(str.length - backChars);
+}
+
+/**
+ * Truncates a string in the middle if it exceeds the desired length; otherwise, extends the resulting string by
+ * padding it at the end to achieve the exact desired length.
+ *
+ * @internal
+ * @param str The input string to be truncated or extended.
+ * @param length The desired exact length of the resulting string.
+ * @returns The truncated or extended string.
+ */
+export function truncateOrExtend(str: string, length: number): string {
+  return truncateMiddle(str, length).padEnd(length, ' ');
+}
