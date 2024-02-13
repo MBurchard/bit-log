e46cc6fbeb2cee95f20c586272444a7ce09dcb83
Martin Burchard <dev@burchard-it.de>
2024-02-13 13:21:49 +0100

    Logger implemented

---
+++ src/__tests__/logger.spec.ts
@@ -0,0 +1,245 @@
+import type {IAppender} from '../definitions';
+import {LogLevel} from '../definitions';
+import {Logger} from '../logger';
+
+describe('test logger', () => {
+  let mockAppender: IAppender;
+
+  beforeEach(() => {
+    mockAppender = {
+      level: LogLevel.INFO,
+      handle: jest.fn().mockResolvedValue(undefined),
+      willHandle: jest.fn().mockReturnValue(true),
+    };
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // Wiederherstellen aller Mocks nach jedem Test
+  });
+
+  describe('initialization', () => {
+    it('without loglevel', () => {
+      const logger = new Logger('');
+      expect(logger.name).toBe('');
+      expect(logger.level).toBe(LogLevel.ERROR);
+      expect(logger);
+    });
+
+    it('with loglevel', () => {
+      const logger = new Logger('foo.bar', undefined, LogLevel.INFO);
+      expect(logger.name).toBe('foo.bar');
+      expect(logger.level).toBe(LogLevel.INFO);
+      expect(logger);
+    });
+  });
+
+  describe('appender handling', () => {
+    let logger: Logger;
+
+    beforeEach(() => {
+      logger = new Logger('foo.bar', undefined, LogLevel.INFO);
+    });
+
+    it('add new appender', () => {
+      // given
+      const name = 'MockAppender';
+
+      expect(logger.addAppender(name, mockAppender)).toBe(true);
+
+      expect(logger.appender[name]).toBe(mockAppender);
+    });
+
+    it('add already registered appender', () => {
+      // given
+      const name = 'MockAppender';
+      logger.addAppender(name, mockAppender);
+
+      expect(logger.addAppender(name, mockAppender)).toBe(false);
+    });
+
+    it('force add appender', () => {
+      // given
+      const name = 'TestAppender';
+      logger.addAppender(name, mockAppender);
+      const appender2: IAppender = {
+        level: LogLevel.DEBUG,
+        handle: async () => {
+        },
+        willHandle: () => {
+          return false;
+        },
+      };
+
+      // when/then
+      expect(logger.addAppender(name, appender2, true)).toBe(true);
+      expect(logger.appender[name]).not.toBe(mockAppender);
+    });
+
+    it('added appender should be used', () => {
+      // given
+      logger.addAppender('MockAppender', mockAppender);
+      const spyEmit = jest.spyOn(logger, 'emit');
+
+      // when
+      logger.info('test info');
+
+      // then
+      expect(mockAppender.willHandle).toHaveBeenCalledTimes(1);
+      expect(mockAppender.handle).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveReturnedWith(true);
+    });
+
+    it('removed appender should not be used', () => {
+      // given
+      logger.addAppender('MockAppender', mockAppender);
+
+      // when
+      expect(logger.removeAppender('MockAppender')).toBe(true);
+      logger.info('test info');
+
+      // then
+      expect(mockAppender.willHandle).toHaveBeenCalledTimes(0);
+      expect(mockAppender.handle).toHaveBeenCalledTimes(0);
+    });
+
+    it('remove appender that was not registered previously', () => {
+      expect(logger.removeAppender('Test')).toBe(false);
+    });
+
+    it('appender exception during handle is caught', async () => {
+      (mockAppender.handle as jest.Mock).mockResolvedValue(Promise.reject('Reject for some reason'));
+
+      logger.addAppender('MockAppender', mockAppender);
+      const consoleErrorSpy =
+        jest.spyOn(console, 'error').mockImplementation(() => {
+        });
+
+      expect(() => logger.info('test info')).not.toThrow();
+
+      await new Promise((r) => setTimeout(r, 10));
+
+      expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+      expect(consoleErrorSpy).toHaveBeenCalledWith('error in appender.handle of MockAppender', 'Reject for some reason');
+    });
+  });
+
+  describe('parent handling', () => {
+    let root: Logger;
+
+    beforeEach(() => {
+      root = new Logger('', undefined, LogLevel.TRACE);
+    });
+
+    it('should get the log event', () => {
+      const logger = new Logger('foo', root, LogLevel.INFO);
+      const spyEmit = jest.spyOn(root, 'emit');
+
+      const now = Date.now();
+      logger.info('Test info');
+
+
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.INFO,
+        loggerName: 'foo',
+        payload: ['Test info'],
+        timestamp: expect.any(Date),
+      });
+      const timestamp = spyEmit.mock.calls[0][0].timestamp;
+      expect(timestamp.getTime()).toBeGreaterThanOrEqual(now);
+      expect(timestamp.getTime()).toBeLessThanOrEqual(Date.now());
+    });
+  });
+
+  describe('logging methods', () => {
+    let logger: Logger;
+    let spyEmit: jest.SpyInstance;
+
+    beforeEach(() => {
+      logger = new Logger('TestLogger', undefined, LogLevel.TRACE);
+      spyEmit = jest.spyOn(logger, 'emit');
+    });
+
+    // noinspection DuplicatedCode
+    it('should trace', () => {
+      logger.trace('test trace');
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.TRACE,
+        loggerName: 'TestLogger',
+        payload: ['test trace'],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    it('should debug', () => {
+      logger.debug('test debug');
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.DEBUG,
+        loggerName: 'TestLogger',
+        payload: ['test debug'],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    // noinspection DuplicatedCode
+    it('should info', () => {
+      logger.info('test info');
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.INFO,
+        loggerName: 'TestLogger',
+        payload: ['test info'],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    it('should warn', () => {
+      logger.warn('test warn');
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.WARN,
+        loggerName: 'TestLogger',
+        payload: ['test warn'],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    it('should error', () => {
+      logger.error('test error', new Error('CustomError'));
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.ERROR,
+        loggerName: 'TestLogger',
+        payload: ['test error', new Error('CustomError')],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    it('should fatal', () => {
+      logger.fatal('test fatal');
+      expect(spyEmit).toHaveBeenCalledTimes(1);
+      expect(spyEmit).toHaveBeenCalledWith({
+        level: LogLevel.FATAL,
+        loggerName: 'TestLogger',
+        payload: ['test fatal'],
+        timestamp: expect.any(Date),
+      });
+    });
+
+    it('should execute log method parameter', () => {
+      const mockLog = jest.fn().mockReturnValue('jest mock log function');
+      logger.debug(mockLog);
+      expect(mockLog).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not execute log method parameter, if log level is to low', () => {
+      logger.level = LogLevel.ERROR;
+      const mockLog = jest.fn().mockReturnValue('jest mock log function');
+      logger.debug(mockLog);
+      expect(mockLog).toHaveBeenCalledTimes(0);
+    });
+  });
+});
---
+++ src/logger.ts
@@ -0,0 +1,178 @@
+/**
+ * src/logger.ts
+ *
+ * @file A hierarchic Logger implementation
+ * @author Martin Burchard
+ */
+import type {IAppender, ILogEvent, ILogger} from './definitions';
+import {isPresent, LogLevel} from './definitions';
+
+/**
+ * A hierarchic Logger implementation
+ *
+ * @internal
+ */
+export class Logger implements ILogger {
+  private readonly parent?: ILogger;
+  readonly appender: Record<string, IAppender> = {};
+  level: LogLevel;
+  readonly name: string;
+
+  /**
+   * This Constructor is not intended to be used externally.
+   *
+   * @internal
+   * @param {string} name the logger name, '' is the root logger
+   * @param {Logger} parent All except the root logger have a parent
+   * @param {LogLevel} level defaults to LogLevel.ERROR
+   */
+  constructor(name: string, parent?: ILogger, level?: LogLevel) {
+    this.name = name;
+    this.parent = parent;
+    this.level = level ?? parent?.level ?? LogLevel.ERROR;
+  }
+
+  private logEvent(level: LogLevel, ...args: unknown[]) {
+    if (!this.shouldLog(level)) {
+      return;
+    }
+
+    let payload;
+    if (typeof args[0] === 'function') {
+      payload = args[0]();
+    } else {
+      payload = args;
+    }
+
+    const event: ILogEvent = {
+      level,
+      loggerName: this.name,
+      payload,
+      timestamp: new Date(),
+    };
+    this.emit(event);
+  }
+
+  /**
+   * Checks if the given LogLevel is processed by this or a parent logger.
+   *
+   * @private
+   * @param {LogLevel} level
+   * @return {boolean} true if this LogLevel should be logged
+   */
+  shouldLog(level: LogLevel): boolean {
+    return level >= this.level;
+  }
+
+  /**
+   * Add an appender.
+   * Appender is only added, if the given name is not already registered at this logger instance or if overwrite is set
+   * to true
+   *
+   * @param {string} appenderName name that is used to register the appender
+   * @param {IAppender} appender the appender to register
+   * @param {boolean} overwrite if a registered appender should be overwritten
+   * @return {boolean} true if the given appender has been registered to this logger
+   */
+  addAppender(appenderName: string, appender: IAppender, overwrite: boolean = false): boolean {
+    if (overwrite || !(appenderName in this.appender)) {
+      this.appender[appenderName] = appender;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Remove the appender, that has been registered before with that given name.
+   *
+   * @param {string} appenderName
+   * @return {boolean} true if the appender has been removed, false if the given name was not registered
+   */
+  removeAppender(appenderName: string): boolean {
+    if (appenderName in this.appender) {
+      delete this.appender[appenderName];
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Log something with LogLevel.DEBUG.
+   * This method has the same signature as the well-known console.log.
+   * Typical loggers or appender will probably, like console.log, combine the given parameters into a string.
+   * Of course, this depends on the implementation of the registered appender.
+   * If the first parameter is a function, it is executed by the logger as soon as it's certain that the log will not
+   * be ignored.
+   *
+   * @param args
+   */
+  debug(...args: unknown[]) {
+    this.logEvent(LogLevel.DEBUG, ...args);
+  }
+
+  /**
+   * @see {@link debug}
+   * @param args
+   */
+  error(...args: unknown[]) {
+    this.logEvent(LogLevel.ERROR, ...args);
+  }
+
+  /**
+   * @see {@link debug}
+   * @param args
+   */
+  fatal(...args: unknown[]) {
+    this.logEvent(LogLevel.FATAL, ...args);
+  }
+
+  /**
+   * @see {@link debug}
+   * @param args
+   */
+  info(...args: unknown[]) {
+    this.logEvent(LogLevel.INFO, ...args);
+  }
+
+  /**
+   * This method is used to emit a LogEvent to the registered handlers and the parent logger.
+   * The method does not check the log level, this was done beforehand.
+   * If this logger has no registered appender or none of the appender want to handle the LogEvent, it is passed "up"
+   * to a possibly existing parent logger.
+   *
+   * @internal
+   * @param {ILogEvent} event
+   * @return {boolean} true if a registered handler will potentially handle this log event
+   */
+  emit(event: ILogEvent): boolean {
+    let handled = false;
+    for (const [appenderName, appender] of Object.entries(this.appender)) {
+      if (appender.willHandle(event)) {
+        appender.handle(event).catch(reason => {
+          console.error(`error in appender.handle of ${appenderName}`, reason);
+        });
+        handled = true;
+      }
+    }
+    if (!handled && isPresent(this.parent) && this.parent instanceof Logger) {
+      return this.parent.emit(event);
+    }
+    return handled;
+  }
+
+  /**
+   * @see {@link debug}
+   * @param args
+   */
+  trace(...args: unknown[]) {
+    this.logEvent(LogLevel.TRACE, ...args);
+  }
+
+  /**
+   * @see {@link debug}
+   * @param args
+   */
+  warn(...args: unknown[]) {
+    this.logEvent(LogLevel.WARN, ...args);
+  }
+}
