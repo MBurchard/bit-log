ee2babff74401a0b82df2a77ef92d757fbc9a2cd
Martin Burchard <dev@burchard-it.de>
2024-02-13 16:37:32 +0100

    add methods `configureLogging` and `useLogger`

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,95 @@
+import {ConsoleAppender} from '../appender/ConsoleAppender';
+import type {LoggingConfig} from '../definitions';
+import {LogLevel} from '../definitions';
+import {configureLogging, useLogger} from '../index';
+import {Logger} from '../logger';
+
+describe('test default configuration', () => {
+  // get the root logger
+  const log = useLogger('');
+  let spyConsole: jest.SpyInstance;
+
+  beforeEach(() => {
+    spyConsole = jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // Wiederherstellen aller Mocks nach jedem Test
+  });
+
+  it('log.trace', () => {
+    log.trace('trace log');
+    expect(spyConsole).toHaveBeenCalledTimes(0);
+  });
+
+  it('log.debug', () => {
+    log.debug('debug log');
+    expect(spyConsole).toHaveBeenCalledTimes(0);
+  });
+
+  it('log.info', () => {
+    log.info('info log');
+    expect(spyConsole).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('useLogger', () => {
+  it('default root logger', () => {
+    const logger = useLogger('');
+    expect(logger).toEqual({
+        level: LogLevel.INFO,
+        name: '',
+        parent: undefined,
+        appender: {
+          'CONSOLE': expect.any(ConsoleAppender),
+        },
+      }
+    );
+  });
+
+  it('logger creation works recursive', () => {
+    const logger = useLogger('foo.bar');
+    expect(logger).toEqual({
+      appender: {},
+      level: LogLevel.INFO,
+      name: 'foo.bar',
+      parent: {
+        appender: {},
+        level: LogLevel.INFO,
+        name: 'foo',
+        parent: {
+          appender: expect.anything(),
+          level: LogLevel.INFO,
+          name: '',
+          parent: undefined,
+        },
+      },
+    });
+  });
+});
+
+describe('test logging configuration', () => {
+  const config: LoggingConfig = {
+    appender: {
+      'CONSOLE': {
+        class: ConsoleAppender,
+        level: LogLevel.DEBUG,
+      },
+    },
+  };
+
+  it('default root logger', () => {
+    const log = useLogger('');
+    expect(log).toBeDefined();
+    expect(log).toBeInstanceOf(Logger);
+    expect(log.level).toBe(LogLevel.INFO);
+    expect((log as Logger).appender).toStrictEqual({'CONSOLE': new ConsoleAppender(LogLevel.TRACE)});
+  });
+
+  it('setup appender', () => {
+    // change log level off ROOT logger to debug, to see logging setup messages
+    const root = useLogger('');
+    root.level = LogLevel.DEBUG;
+    configureLogging(config);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+/**
+ * src/index.ts
+ *
+ * @file Bit-Log
+ * @author Martin Burchard
+ */
+
+import {ConsoleAppender} from './appender/ConsoleAppender';
+import type {IAppender, ILogger, LoggingConfig} from './definitions';
+import {isPresent, LogLevel} from './definitions';
+import {Logger} from './logger';
+
+const ROOT = new Logger('', undefined, LogLevel.INFO);
+const LoggerRegistry: Record<string, ILogger> = {};
+const AppenderRegistry: Record<string, IAppender> = {};
+const CONSOLE = 'CONSOLE';
+AppenderRegistry[CONSOLE] = new ConsoleAppender(LogLevel.INFO);
+ROOT.addAppender(CONSOLE, AppenderRegistry[CONSOLE]);
+
+/**
+ * used to configure the logging. Should be used as early ass possible.
+ * See also: @see {@link LoggingConfig}
+ *
+ * @param {LoggingConfig} config
+ */
+export function configureLogging(config: LoggingConfig): void {
+  ROOT.debug('configure logging');
+  if (isPresent(config.appender)) {
+    ROOT.debug('configure appender');
+    for (const [appenderName, appenderConfig] of Object.entries(config.appender)) {
+      if (appenderConfig.class && typeof appenderConfig.class === 'function' && appenderConfig.class.prototype instanceof Object) {
+        try {
+          const instance = new appenderConfig.class();
+          for (const [key, value] of Object.entries(appenderConfig)) {
+            if (key !== 'class') {
+              instance[key as keyof IAppender] = value;
+            }
+          }
+          if (appenderName in AppenderRegistry) {
+            ROOT.debug(`found existing appender: ${appenderName}, search and replace it`);
+            if (appenderName in ROOT.appender) {
+              ROOT.debug('Replace appender', appenderName, 'in ROOT logger');
+              ROOT.addAppender(appenderName, instance, true);
+            }
+            for (const [loggerName, logger] of Object.entries(LoggerRegistry)) {
+              if (!(logger instanceof Logger)) {
+                continue;
+              }
+              if (appenderName in logger.appender) {
+                ROOT.debug('Replace appender ', appenderName, 'in logger', loggerName);
+                logger.addAppender(appenderName, instance, true);
+              }
+            }
+          }
+          AppenderRegistry[appenderName] = instance;
+        } catch (e) {
+          throw Error(`illegal appender config ${JSON.stringify(appenderConfig)}, error: ${e}`);
+        }
+      } else {
+        throw Error(`illegal appender config ${JSON.stringify(appenderConfig)}`);
+      }
+    }
+  }
+  if (isPresent(config.root)) {
+    ROOT.debug('configure ROOT logger');
+
+  }
+  if (isPresent(config.logger)) {
+    ROOT.debug('configure additional loggers');
+  }
+}
+
+export function useLogger(name: string): ILogger {
+  if (name === '') {
+    return ROOT;
+  }
+  if (!(name in LoggerRegistry)) {
+    const parentName = name.includes('.') ? name.substring(0, name.lastIndexOf('.')) : '';
+    const parent = useLogger(parentName);
+    const logger = new Logger(name, parent as Logger);
+    LoggerRegistry[name] = logger;
+    return logger;
+  }
+  return LoggerRegistry[name];
+}
