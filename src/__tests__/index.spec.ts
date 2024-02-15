54aa64c412dd6a894162d9146983d93a31f0df8c
Martin Burchard <dev@burchard-it.de>
2024-02-15 21:26:05 +0100

    tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,95 +1,122 @@
+import {AbstractBaseAppender} from '../appender/AbstractBaseAppender';
 import {ConsoleAppender} from '../appender/ConsoleAppender';
 import type {LoggingConfig} from '../definitions';
 import {LogLevel} from '../definitions';
 import {configureLogging, useLogger} from '../index';
-import {Logger} from '../logger';
 
-describe('test default configuration', () => {
-  // get the root logger
-  const log = useLogger('');
+describe('test usage', () => {
   let spyConsole: jest.SpyInstance;
 
   beforeEach(() => {
+    jest.resetModules();
     spyConsole = jest.spyOn(console, 'log').mockImplementation();
   });
 
   afterEach(() => {
-    jest.restoreAllMocks(); // Wiederherstellen aller Mocks nach jedem Test
+    jest.restoreAllMocks();
   });
 
-  it('log.trace', () => {
-    log.trace('trace log');
-    expect(spyConsole).toHaveBeenCalledTimes(0);
+  describe('test default configuration', () => {
+    // get the root logger
+    const log = useLogger('');
+
+    it('log.trace', () => {
+      log.trace('trace log');
+      expect(spyConsole).toHaveBeenCalledTimes(0);
+    });
+
+    it('log.debug', () => {
+      log.debug('debug log');
+      expect(spyConsole).toHaveBeenCalledTimes(0);
+    });
+
+    it('log.info', () => {
+      log.info('info log');
+      expect(spyConsole).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('log.debug', () => {
-    log.debug('debug log');
-    expect(spyConsole).toHaveBeenCalledTimes(0);
-  });
-
-  it('log.info', () => {
-    log.info('info log');
-    expect(spyConsole).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe('useLogger', () => {
-  it('default root logger', () => {
-    const logger = useLogger('');
-    expect(logger).toEqual({
-        level: LogLevel.INFO,
-        name: '',
-        parent: undefined,
-        appender: {
-          'CONSOLE': expect.any(ConsoleAppender),
-        },
-      }
-    );
-  });
-
-  it('logger creation works recursive', () => {
-    const logger = useLogger('foo.bar');
-    expect(logger).toEqual({
-      appender: {},
-      level: LogLevel.INFO,
-      name: 'foo.bar',
-      parent: {
-        appender: {},
-        level: LogLevel.INFO,
-        name: 'foo',
-        parent: {
-          appender: expect.anything(),
+  describe('useLogger', () => {
+    it('default root logger', () => {
+      const logger = useLogger('');
+      expect(logger).toEqual({
           level: LogLevel.INFO,
           name: '',
           parent: undefined,
+          appender: {
+            'CONSOLE': expect.any(ConsoleAppender),
+          },
+        }
+      );
+    });
+
+    it('logger creation works recursive', () => {
+      const logger = useLogger('foo.bar');
+      expect(logger).toEqual({
+        appender: {},
+        level: LogLevel.INFO,
+        name: 'foo.bar',
+        parent: {
+          appender: {},
+          level: LogLevel.INFO,
+          name: 'foo',
+          parent: {
+            appender: expect.anything(),
+            level: LogLevel.INFO,
+            name: '',
+            parent: undefined,
+          },
+        },
+      });
+    });
+  });
+
+  describe('test logging configuration', () => {
+    const config: LoggingConfig = {
+      appender: {
+        'CONSOLE': {
+          class: ConsoleAppender,
+          level: LogLevel.DEBUG,
         },
       },
+      root: {
+        level: LogLevel.DEBUG,
+      },
+      logger: {},
+    };
+
+    it('setup appender', () => {
+      configureLogging(config);
+    });
+
+    it('appender may throw errors on initialization', () => {
+      try {
+        configureLogging({
+          appender: {
+            'SOMETEST': {
+              class: ErrorThrowingAppender,
+              someKey: 100,
+              otherKey: 'Hallo Welt',
+            },
+          },
+        });
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error);
+        expect(e.message).toBe('illegal appender config {class: [class ErrorThrowingAppender extends ' +
+          'AbstractBaseAppender], someKey: 100, otherKey: "Hallo Welt"}, error: Error: Something was wrong');
+      }
     });
   });
 });
 
-// describe('test logging configuration', () => {
-//   const config: LoggingConfig = {
-//     appender: {
-//       'CONSOLE': {
-//         class: ConsoleAppender,
-//         level: LogLevel.DEBUG,
-//       },
-//     },
-//   };
-//
-//   it('default root logger', () => {
-//     const log = useLogger('');
-//     expect(log).toBeDefined();
-//     expect(log).toBeInstanceOf(Logger);
-//     expect(log.level).toBe(LogLevel.INFO);
-//     expect((log as Logger).appender).toStrictEqual({'CONSOLE': new ConsoleAppender(LogLevel.TRACE)});
-//   });
-//
-//   it('setup appender', () => {
-//     // change log level off ROOT logger to debug, to see logging setup messages
-//     const root = useLogger('');
-//     root.level = LogLevel.DEBUG;
-//     configureLogging(config);
-//   });
-// });
+class ErrorThrowingAppender extends AbstractBaseAppender {
+
+  constructor() {
+    super();
+    throw Error('Something was wrong');
+  }
+
+  handle(): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+}
