a85de847e2adf9d1b691c0498fe2d40c6b1154b9
Martin Burchard <dev@burchard-it.de>
2024-05-08 21:29:48 +0200

    add formatting methods that can be overwritten on any instance

--- src/appender/AbstractBaseAppender.ts
+++ src/appender/AbstractBaseAppender.ts
@@ -1,8 +1,25 @@
 import type {IAppender, ILogEvent, LogLevel} from '../definitions.js';
 import {isPresent} from '../definitions.js';
+import {formatAny, formatISO8601, formatLogLevel, truncateOrExtend} from '../utils.js';
 
 export abstract class AbstractBaseAppender implements IAppender {
   level?: LogLevel;
+
+  formatAny = formatAny;
+
+  formatLogLevel = formatLogLevel;
+
+  formatPrefix(ts: Date, level: LogLevel, name: string, colored: boolean = false): string {
+    let formattedLevel;
+    if (colored) {
+      formattedLevel = this.formatLogLevel(level, colored).padStart(13, ' ');
+    } else {
+      formattedLevel = this.formatLogLevel(level).padStart(5, ' ');
+    }
+    return `${this.formatTimestamp(ts)} ${formattedLevel} [${truncateOrExtend(name, 20)}]:`;
+  }
+
+  formatTimestamp = formatISO8601;
 
   abstract handle(event: ILogEvent): Promise<void>;
 
